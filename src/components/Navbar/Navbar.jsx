42774014a6f049e419b95c6b6373a314dd3b7590
brendonvan <brendon@van.digital>
2025-04-12 12:25:34 -0700

    feat: Add Hero and troubleshooted background img

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,10 +14,10 @@ function Navbar() {
       <div className="h-[83px] px-4 flex items-center justify-between bg-[var(--ui-color-light)]">
         {/* Nav Left Side */}
         <div className="flex items-center justify-start">
-          <img src="/images/nasm-logo-blue.svg" alt="NASM Logo" className="w-[5.5rem]"/>
-          <img src="/images/nasm-menu.png" alt="Menu" onClick={menuToggle} className="w-[3rem] mx-4"/>
+          <img src="/images/nav/nasm-logo-blue.svg" alt="NASM Logo" className="w-[5.5rem]"/>
+          <img src="/images/nav/nasm-menu.png" alt="Menu" onClick={menuToggle} className="w-[3rem] mx-4"/>
           <a href="tel:+18004606276">
-            <img src="/images/nasm-mobile.png" alt="Phone" className="w-[2rem]"/>
+            <img src="/images/nav/nasm-mobile.png" alt="Phone" className="w-[2rem]"/>
           </a>
         </div>
 
@@ -25,7 +25,7 @@ function Navbar() {
         <div className="flex items-center justify-end">
           <button onClick={signInButton} className="px-4 pt-[9px] pb-[12px] text-[14px] rounded-[25px] bg-[var(--brand-color-dark)] leading-none">SIGN IN</button>
           <a href="https://shop.nasm.org/cart">
-            <img src="/images/nasm-shopping-cart.png" alt="Cart" className="w-[2.5rem] ml-4 mb-2"/>
+            <img src="/images/nav/nasm-shopping-cart.png" alt="Cart" className="w-[2.5rem] ml-4 mb-2"/>
           </a>
         </div>
       </div>
