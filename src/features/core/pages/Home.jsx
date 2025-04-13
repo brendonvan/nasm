306475f3e15d95e0616a50c506cd4f7e80f2b597
brendonvan <brendon@van.digital>
2025-04-13 12:54:34 -0700

    feature/ Style mobile view hero

--- src/features/core/pages/Home.jsx
+++ src/features/core/pages/Home.jsx
@@ -5,15 +5,26 @@ function Home() {
     <>
       <section
         id="hero"
-        className="h-[680px] bg-cover bg-center bg-no-repeat bg-[url('/images/hero/background-mobile.jpg')] lg:bg-[url('/images/hero/background-desktop.jpg')]"
+        className="h-[680px] px-[1rem] text-center bg-cover bg-center bg-no-repeat bg-[url('/images/hero/background-mobile.jpg')] lg:bg-[url('/images/hero/background-desktop.jpg')]"
       >
-        <div>
-          <div>
-            <p>Trusted by 1.5M+ Pros</p>
-            <h2>Build Your Future With the Industry Leader</h2>
-            <p>Join the world's top fitness professionals. For a limited time, save up to 50%</p>
+        <div className="h-[38rem] pt-[3rem] flex flex-col justify-between">
+          <div className="flex flex-col items-center gap-4">
+            <p className="uppercase tracking-[0.08rem] leading-none">
+              Trusted Worldwide. Built for What's Next.
+            </p>
+            <h2 className="uppercase text-[2.5rem] font-var-extrabold-condensed leading-none">
+              Get NASM Certified
+              <br />
+              Up to 50% Off
+            </h2>
+            <p className="w-[80%] text-[18px] tracking-[0.08rem] leeading-[27px]">
+              Join 1.5M+ professionals who choose NASM to lead the way in
+              fitness and wellness. A new season. A stronger you.
+            </p>
           </div>
-          <a href=""><span>Save up to 50%</span></a>
+          <a href="" className="nasm-primary-btn text-[1.25rem]!">
+            <span>Get Started Today</span>
+          </a>
         </div>
       </section>
     </>
